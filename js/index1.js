b3a1acf29746880dacd7894deff5201b20c73d5f
Manish <mkraj13579@gmail.com>
2018-11-22 22:11:25 +0530

    Update index1.js

--- js/index1.js
+++ js/index1.js
@@ -1,5 +1,49 @@
+var config = {
+    apiKey: "AIzaSyACfS-zcajoZ_AGrOSr3bQQf9ikAYDaaFI",
+    authDomain: "geomap-a240a.firebaseapp.com",
+    databaseURL: "https://geomap-a240a.firebaseio.com",
+    projectId: "geomap-a240a",
+    storageBucket: "",
+    messagingSenderId: "92013339379"
+};
+firebase.initializeApp(config);
+var firebasedata = firebase.database().ref();
+var watchID;
+var geoLoc;
 
 
+function toggle(input) {
+    if (input.value == "OFF") {
+        stopWatch();
+        input.value = "ON";
+        
+    } else {
+        getLocationUpdate();
+        
+        input.value = "OFF";
+    }
+}
+
+
+function showLocation(position) {
+    var latitude = position.coords.latitude;
+    var longitude = position.coords.longitude;
+    var accuracy = position.coords.accuracy;
+    firebasedata.child('ac1/bus1').set({
+        "geometry": { "type": "Point", "coordinates": [longitude, latitude] }, "type": "Feature", "properties": {}
+        
+    });
+    
+
+}
+
+function errorHandler(err) {
+    if (err.code == 1) {
+        alert("Error: Access is denied!");
+    } else if (err.code == 2) {
+        alert("Error: Position is unavailable!");
+    }
+}
 
 function getLocationUpdate() {
 
